cf439e66ece0ee7df74e684d942cb3cf6af2d460
Andrii Zakharenko <andriizakharenko@gmail.com>
2025-01-04 20:33:58 +0200

    feat: Admin Page ( table)

--- components/table/columns.tsx
+++ components/table/columns.tsx
@@ -1,7 +1,103 @@
 "use client";
+import { Doctors } from "@/constants/components/doctors";
+import { formatDateTime } from "@/lib/utils";
+import { Appointment } from "@/types/appwrite.types";
+import { ColumnDef } from "@tanstack/react-table";
+import Image from "next/image";
+import StatusBadge from "../StatusBadge";
+import AppointmentModal from "../AppointmentModal";
 
-const Columns = () => {
-  return <div>Columns</div>;
-};
+export const columns: ColumnDef<Appointment>[] = [
+  {
+    header: "#",
+    cell: ({ row }) => {
+      return <p className="text-14-medium ">{row.index + 1}</p>;
+    },
+  },
+  {
+    accessorKey: "patient",
+    header: "Patient",
+    cell: ({ row }) => {
+      const appointment = row.original;
+      return <p className="text-14-medium ">{appointment.patient.name}</p>;
+    },
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: ({ row }) => {
+      const appointment = row.original;
+      return (
+        <div className="min-w-[115px]">
+          <StatusBadge status={appointment.status} />
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "schedule",
+    header: "Appointment",
+    cell: ({ row }) => {
+      const appointment = row.original;
+      return (
+        <p className="text-14-regular min-w-[100px]">
+          {formatDateTime(appointment.schedule).dateTime}
+        </p>
+      );
+    },
+  },
+  {
+    accessorKey: "primaryPhysician",
+    header: "Doctor",
+    cell: ({ row }) => {
+      const appointment = row.original;
 
-export default Columns
+      const doctor = Doctors.find(
+        (doctor) => doctor.name === appointment.primaryPhysician
+      );
+
+      return doctor ? (
+        <div className="flex items-center gap-3">
+          <Image
+            src={doctor.image || "/assets/images/default-doctor.png"}
+            alt={`Doctor ${doctor.name}`}
+            width={100}
+            height={100}
+            className="size-8"
+          />
+          <p className="whitespace-nowrap">Dr. {doctor.name}</p>
+        </div>
+      ) : (
+        <p className="text-red-500">Doctor not found</p>
+      );
+    },
+  },
+  {
+    id: "actions",
+    header: () => <div className="pl-4">Actions</div>,
+    cell: ({ row }) => {
+      const appointment = row.original;
+
+      return (
+        <div className="flex gap-1">
+          <AppointmentModal
+            patientId={appointment.patient.$id}
+            userId={appointment.userId}
+            appointment={appointment}
+            type="schedule"
+            title="Schedule Appointment"
+            description="Please confirm the following details to schedule."
+          />
+          <AppointmentModal
+            patientId={appointment.patient.$id}
+            userId={appointment.userId}
+            appointment={appointment}
+            type="cancel"
+            title="Cancel Appointment"
+            description="Are you sure you want to cancel your appointment?"
+          />
+        </div>
+      );
+    },
+  },
+];
